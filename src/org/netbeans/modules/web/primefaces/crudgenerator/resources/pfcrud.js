621845a26408b82d2f01fd6fd51714211711c2f4
Kay Wrobel <>
2013-02-18 11:15:31 -0600

    Fix for ticket [#10]. Error condition caused by invalid use of dialog.effect. Must obtain jQueryUI Dialog instead in order to run the `effect()` method.

--- src/org/netbeans/modules/web/primefaces/crudgenerator/resources/pfcrud.js
+++ src/org/netbeans/modules/web/primefaces/crudgenerator/resources/pfcrud.js
@@ -1,6 +1,7 @@
 function handleSubmit(xhr, status, args, dialog) {
+    var jqDialog = jQuery('#'+dialog.id);
     if(args.validationFailed) {
-        dialog.effect('shake', { times:3 }, 100);
+        jqDialog.effect('shake', { times:3 }, 100);
     } else {
         dialog.hide();
     }
